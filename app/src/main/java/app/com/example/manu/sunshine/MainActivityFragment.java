3dc2be20d6d840799d9bb124ee59b2086aba3951
MANU <MANU@iMac-de-MANUEL.local>
2015-08-02 13:42:51 +0200

    connection previous aynctask

--- app/src/main/java/app/com/example/manu/sunshine/MainActivityFragment.java
+++ app/src/main/java/app/com/example/manu/sunshine/MainActivityFragment.java
@@ -9,6 +9,12 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -46,6 +52,82 @@ public class MainActivityFragment extends Fragment {
         ListView miListView = (ListView)rootView.findViewById(R.id.listView_forecast);
 
         miListView.setAdapter(miAdaptadorArray);
+
+//declaro fuera del try-catch para cerrarlos en el bloque finally
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+
+        //respuesta Json como string
+        String forecastJsonStr = null;
+
+        try{
+
+            URL url = new URL("http://api.openweathermap.org/data/2.5/forecast/daily?q=94043&mode=json&units=metric&cnt=7");
+
+            //falta incluir el retardo por ejemplo http://developer.android.com/training/basics/network-ops/connecting.html (setReadTimeout,setConnectTimeout)
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            InputStream inputStream = urlConnection.getInputStream();
+            StringBuffer buffer = new StringBuffer();
+            if (inputStream == null) {
+                // Nothing to do.
+                return null;
+            }
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+                // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                // But it does make debugging a *lot* easier if you print out the completed
+                // buffer for debugging.
+                buffer.append(line + "\n");
+            }
+
+            if (buffer.length() == 0) {
+                // Stream was empty.  No point in parsing.
+                return null;
+            }
+
+
+            forecastJsonStr = buffer.toString();
+
+
+
+
+
+        }catch(IOException ex){
+
+            Log.e("MainActivityFragment", "Error ", ex);
+                          // If the code didn't successfully get the weather data, there's no point in attemping
+                                // to parse it.
+                                        return null;
+
+        }
+
+        //Al final he de cerrar
+
+finally {
+
+
+            if(urlConnection !=null){
+                urlConnection.disconnect();
+            }
+
+            if(reader!=null){
+
+
+                try{
+
+
+                    reader.close();
+                }catch(IOException e){
+
+                    Log.e("PlaceholderFragment", "Error closing stream", e);
+                }
+            }
+        }
 
 
         return rootView;
